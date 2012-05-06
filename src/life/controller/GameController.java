ae017dbf44de95595c1d9c260a7e13d688332ab9
katyg <katy.groves@gmail.com>
2012-05-06 12:39:11 -0400

    Added GameController and build.xml.  Project now builds into an executable jar file.

---
+++ src/life/controller/GameController.java
@@ -0,0 +1,15 @@
+package life.controller;
+
+public class GameController {
+
+	//get user input
+	
+	//loop through game board and apply rules
+	
+	//print new board to screen
+	
+	public static void main(String [] args)
+	{
+		System.out.println("Hello World!"); // Display the string.
+	}
+}
